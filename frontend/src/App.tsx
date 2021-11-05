7c8506a6f307daf37d58149a3f791939e5ff31b0
kauecm <kauecmoraes@hotmail.com>
2021-11-05 14:48:20 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import DataTable from "componets/DataTable";
+import Footer from "componets/Footer";
+import NavBar from "componets/NavBar";
+
 function App() {
   return (
-    <div><h1>ola mundo</h1>
+    <>
+    <NavBar/>
+    <div className="container">
+      <h1 className="text-primary">ola mundo</h1>
+      <DataTable/>
     </div>
+    
+    <Footer/>
+    </>
   );
 }
 
