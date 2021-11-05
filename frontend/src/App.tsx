2a45dbdfb495f6ecfaab049a82685746cbe304db
kauecm <kauecmoraes@hotmail.com>
2021-11-05 17:38:38 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
+import BarCharts from "componets/BarCharts";
 import DataTable from "componets/DataTable";
 import Footer from "componets/Footer";
 import NavBar from "componets/NavBar";
+import PieCharts from "componets/PieCharts";
 
 function App() {
   return (
     <>
     <NavBar/>
     <div className="container">
-      <h1 className="text-primary">ola mundo</h1>
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <BarCharts/>
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Todas Vendas</h5>
+          <PieCharts/>
+        </div>
+      </div>
+      <div className="py-3"> 
+      <h2 className="text-primary">Todas as Vendas</h2>
+      </div>
       <DataTable/>
     </div>
     
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import 'assets/css/styles.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
