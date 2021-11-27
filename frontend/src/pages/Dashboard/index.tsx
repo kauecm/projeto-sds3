d527cfc35b03ec2adc19da7cb6cc1cf79ef2b37a
kauecm <kauecmoraes@hotmail.com>
2021-11-27 20:31:19 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,34 @@
+import BarCharts from "componets/BarCharts";
+import DataTable from "componets/DataTable";
+import Footer from "componets/Footer";
+import NavBar from "componets/NavBar";
+import PieCharts from "componets/PieCharts";
+
+function Dashboard() {
+    return (
+        <>
+    <NavBar/>
+    <div className="container">
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <BarCharts/>
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Todas Vendas</h5>
+          <PieCharts/>
+        </div>
+      </div>
+      <div className="py-3"> 
+      <h2 className="text-primary">Todas as Vendas</h2>
+      </div>
+      <DataTable/>
+    </div>
+    
+    <Footer/>
+    </>
+    );
+  }
+  
+  export default Dashboard;
