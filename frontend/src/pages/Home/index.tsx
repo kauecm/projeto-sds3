e86f5b4998474e41eea6aab1bdb1de977467e48b
kauecm <kauecmoraes@hotmail.com>
2021-11-27 20:41:19 -0300

    home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,23 @@
+import Footer from "componets/Footer";
+import NavBar from "componets/NavBar";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div>
-            <h1>Página Home</h1>        
-      </div>
+        <>
+        <NavBar/>
+        <div className="container">
+        <div className="jumbotron">
+            <h1 className="display-4">DSVendas</h1>
+            <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+            <hr/>
+            <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+            <Link className="btn btn-primary btn-lg" to="/dashboard">Dashboard</Link>
+        </div>
+    </div>
+
+        <Footer/>
+    </>
     );
   }
   
