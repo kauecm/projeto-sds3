1ba830ce7fa84f27d3844ee8c798cceb553e770d
kauecm <kauecmoraes@hotmail.com>
2021-11-27 21:06:31 -0300

    First request

--- frontend/src/componets/PieCharts/index.tsx
+++ frontend/src/componets/PieCharts/index.tsx
@@ -1,9 +1,35 @@
+import axios from 'axios';
+import { type } from 'os';
 import Chart from 'react-apexcharts'
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/request';
+
+type ChartData = {
+    labels: string[];
+    series: number[]; 
+}
+
+
 function PieCharts() {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    //FORMA ERRADA
+    let chartData : ChartData = {labels: [], series:[] };
+
+    axios.get(`${BASE_URL}/sales/sumBySeller`)
+        .then(response =>{
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = {labels : myLabels, series:mySeries }
+
+            console.log(response.data);
+
+        });
+
+   // const mockData = {
+   //     series: [477138, 499928, 444867, 220426, 473088],
+   //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+   // }
     
     const options = {
         legend: {
@@ -13,8 +39,8 @@ function PieCharts() {
   
     return (
         <Chart 
-        options={{ ...options, labels:mockData.labels}}
-        series={mockData.series}
+        options={{ ...options, labels: chartData.labels}}
+        series={chartData.series}
         type="pie"
         height="240"
         />
