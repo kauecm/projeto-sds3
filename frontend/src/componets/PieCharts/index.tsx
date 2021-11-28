7a242fd9a68146ada5577637aed0c39a5e0961ac
kauecm <kauecmoraes@hotmail.com>
2021-11-27 21:17:29 -0300

    PieChart Integration

--- frontend/src/componets/PieCharts/index.tsx
+++ frontend/src/componets/PieCharts/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { type } from 'os';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
@@ -11,20 +11,24 @@ type ChartData = {
 
 
 function PieCharts() {
-    //FORMA ERRADA
-    let chartData : ChartData = {labels: [], series:[] };
 
-    axios.get(`${BASE_URL}/sales/sumBySeller`)
+    const [chartData, setChartData] = useState<ChartData>({labels : [], series : []});
+    
+
+    useEffect(()=>{
+        axios.get(`${BASE_URL}/sales/sumBySeller`)
         .then(response =>{
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartData = {labels : myLabels, series:mySeries }
-
-            console.log(response.data);
+            setChartData({labels : myLabels, series : mySeries });
+            
 
         });
+    } ,[]);
+
+    
 
    // const mockData = {
    //     series: [477138, 499928, 444867, 220426, 473088],
